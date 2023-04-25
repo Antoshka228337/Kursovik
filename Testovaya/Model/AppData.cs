6bc3b06e7bd70d2af0a3d412f1e407a4cbfe6802
Anton <lavrovserguna@mail.ru>
2023-04-25 20:31:19 +0300

    Hursach

---
+++ Testovaya/Model/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testovaya.Model
+{
+    public static class AppData//Связь с БД на которую потом ссылаемся почти в каждом окне приложения
+    {
+        public static PenisEntities db = new PenisEntities();
+    }
+}
